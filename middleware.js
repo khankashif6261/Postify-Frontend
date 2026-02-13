beef270884e29b5e04649e6646811ff75af8e9bb
Kashif Khan <khankashif6261@gmail.com>
2026-02-13 15:40:40 +0530

    changes for secured routes updated middleware

---
+++ middleware.js
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import React, { useEffect, useState } from "react";
+export default function MiddlewareJs (req) {
+    const [isValid, setisValid] = useState(false);
+    useEffect(() => {      
+    const authCheck = async() => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/verify`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      })
+      const data = await res.json();
+      setisValid(data.valid);
+    }
+    authCheck();
+    }, [])
+    try {
+        if (!isValid) {
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
+    else {
+        return NextResponse.next();
+    }
+    }
+    catch (err) {
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
+}
+export const config = 
+{
+    matcher: ["/home/:path", "/expore/:path", "/subtea/:path", "/edit/:path","/AboutUs/:path", "/ContactUs/:path", "/profile/:path"],
+}
