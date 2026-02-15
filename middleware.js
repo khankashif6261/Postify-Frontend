6fc3456dc299bf751771b95a0e56ef6860ed7516
Kashif Khan <khankashif6261@gmail.com>
2026-02-15 14:47:43 +0530

    middleware matcher fixed

--- middleware.js
+++ middleware.js
@@ -18,5 +18,5 @@ export default function MiddlewareJs(req) {
 }
 export const config = 
 {
-    matcher: ["/home", "/expore", "/subtea/:name/:path", "/edit/:path","/AboutUs", "/ContactUs", "/profile"],
+    matcher: ["/home/:path*", "/expore/:path*", "/subtea/:name/:path*", "/edit/:path*","/AboutUs/:path*", "/ContactUs/:path*", "/profile/:path*"],
 }
