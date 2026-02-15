0b48d0d2b07ab7fb1ee381c0eff75aa506a14f78
Kashif Khan <khankashif6261@gmail.com>
2026-02-15 16:05:18 +0530

    next client middleware temporary

--- middleware.js
+++ middleware.js
@@ -1,22 +1,35 @@
-import { NextResponse } from "next/server"
-
-export default function middleware(req) {
-    const token = req.cookies.get("token")?.value;
-    console.log(token);
+"use client"
+import { NextResponse } from "next/server";
+import React, { useEffect, useState } from "react";
+export default function MiddlewareJs (req) {
+    const [isValid, setisValid] = useState(false);
+    useEffect(() => {      
+    const authCheck = async() => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/verify`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      })
+      const data = await res.json();
+      setisValid(data.valid);
+    }
+    authCheck();
+    }, [])
     try {
-        if(!token) {
-        return NextResponse.redirect(new URL("/login", req.url))
+        if (!isValid) {
+        return NextResponse.redirect(new URL("/login", req.url));
     }
     else {
         return NextResponse.next();
     }
     }
-    catch(err) {
-        console.log("Error from catch is: ", err);
-        NextResponse.redirect(new URL("/login",req.url));
+    catch (err) {
+        return NextResponse.redirect(new URL("/login", req.url));
     }
 }
 export const config = 
 {
-    matcher: ["/home/:path*", "/expore/:path*", "/subtea/:name/:path*", "/edit/:path*","/AboutUs/:path*", "/ContactUs/:path*", "/profile/:path*"],
+    matcher: ["/home/:path*", "/explore/:path*", "/subtea/:name/:path*", "/edit/:path*","/AboutUs/:path*", "/ContactUs/:path*", "/profile/:path*"],
 }
