44cf25579303200ce4201168a7101084014dfe2d
Kashif Khan <khankashif6261@gmail.com>
2026-02-15 15:59:14 +0530

    updated middleware post token

--- middleware.js
+++ middleware.js
@@ -1,38 +1,22 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"
 
-export function middleware(req) {
-  try {
-    const token = req.cookies.get("token")?.value;
-    console.log(req.cookies);
-    console.log("Token:", token);
-
-    // If token missing → redirect
-    if (!token) {
-      return NextResponse.redirect(
-        new URL("/login", req.url)
-      );
+export default function middleware(req) {
+    const token = req.cookies.get("token").value;
+    console.log(token);
+    try {
+        if(!token) {
+        return NextResponse.redirect(new URL("/login", req.url))
     }
-
-    // Token exists → allow
-    return NextResponse.next();
-
-  } catch (err) {
-    console.log("Middleware crash:", err);
-
-    return NextResponse.redirect(
-      new URL("/login", req.url)
-    );
-  }
+    else {
+        return NextResponse.next();
+    }
+    }
+    catch(err) {
+        console.log("Error from catch is: ", err);
+        NextResponse.redirect(new URL("/login",req.url));
+    }
 }
-
-export const config = {
-  matcher: [
-    "/home/:path*",
-    "/explore/:path*",
-    "/subtea/:path*",
-    "/edit/:path*",
-    "/AboutUs/:path*",
-    "/ContactUs/:path*",
-    "/profile/:path*",
-  ],
-};
+export const config = 
+{
+    matcher: ["/home/:path", "/expore/:path", "/subtea/:name/:path", "/edit/:path","/AboutUs/:path", "/ContactUs/:path", "/profile/:path"],
+}
