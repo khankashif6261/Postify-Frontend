00c212b30312b80467ce12daafea7151d5aff51f
Kashif Khan <khankashif6261@gmail.com>
2026-04-01 21:52:12 +0530

    attemp to fix backend sleeping issue

--- middleware.js
+++ middleware.js
@@ -1,12 +1,33 @@
 import { NextResponse } from "next/server";
 
-export function middleware(req) {
+export function middleware(req){
 
-    const token = req.cookies.get("token");
+const token = req.cookies.get("token");
+const { pathname } = req.nextUrl;
 
-    if (!token) {
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
-
-    return NextResponse.next();
+// allow public pages
+if(
+pathname === "/login" ||
+pathname === "/register"
+){
+return NextResponse.next();
 }
+
+// protect private pages
+if(!token){
+return NextResponse.redirect(
+new URL("/login",req.url)
+);
+}
+
+return NextResponse.next();
+
+}
+
+export const config = {
+matcher:[
+"/home/:path*",
+"/profile/:path*",
+"/explore/:path*"
+]
+};
