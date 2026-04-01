c87f7d5bee90e2c77d620889414fd72144e83de1
Kashif Khan <khankashif6261@gmail.com>
2026-04-01 21:49:03 +0530

    attempt to fix middleware.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,8 +1,7 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import FullScreenLoader from '../components/FullScreenLoader';
 const Login = () => {
-
   const [name, setname] = useState("");
   const [loading, setloading] = useState(false)
   const [pass, setpass] = useState("")
