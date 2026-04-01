14bd2da9c86c5b3a8fdcd95714b8abd690a77611
Kashif Khan <khankashif6261@gmail.com>
2026-04-01 21:58:08 +0530

    test for cookies from backend

--- middleware.js
+++ middleware.js
@@ -1,33 +1,13 @@
 import { NextResponse } from "next/server";
 
-export function middleware(req){
+export function middleware(req) {
 
-const token = req.cookies.get("token");
-const { pathname } = req.nextUrl;
+    const token = req.cookies.get("token");
+    console.log(req);
 
-// allow public pages
-if(
-pathname === "/login" ||
-pathname === "/register"
-){
-return NextResponse.next();
+    if (!token) {
+        return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    return NextResponse.next();
 }
-
-// protect private pages
-if(!token){
-return NextResponse.redirect(
-new URL("/login",req.url)
-);
-}
-
-return NextResponse.next();
-
-}
-
-export const config = {
-matcher:[
-"/home/:path*",
-"/profile/:path*",
-"/explore/:path*"
-]
-};
